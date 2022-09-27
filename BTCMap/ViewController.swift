f8044d4c89c9ad755055243da964886b3ffcdece
Vitaly Berg <bergusman@gmail.com>
2022-09-28 03:03:59 +0800

    Create empty Xcode project

---
+++ BTCMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BTCMap
+//
+//  Created by Vitaly Berg on 9/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
