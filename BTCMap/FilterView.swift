fee30e0f48a345df9e6672306aaa8cf059d6e030
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2023-03-17 10:15:52 +0900

    Created FilterView

---
+++ BTCMap/FilterView.swift
@@ -0,0 +1,21 @@
+//
+//  FilterView.swift
+//  BTCMap
+//
+//  Created by Lorenzo Primiterra on 16/03/23.
+//
+
+import SwiftUI
+
+struct FilterView: View {
+    var body: some View {
+        HStack {
+        }
+    }
+}
+
+struct FilterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FilterView()
+    }
+}
