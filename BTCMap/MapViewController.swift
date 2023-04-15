0ab11f8c9ad14620999d95ccd969f1ba511bf675
salva <>
2023-04-15 09:45:53 -0400

    Use brighter colors for community polygons

--- BTCMap/MapViewController.swift
+++ BTCMap/MapViewController.swift
@@ -226,9 +226,9 @@ class MapViewController: UIViewController, MKMapViewDelegate, UISheetPresentatio
     
     // MARK: Random Color
     private func randomColor(alpha: CGFloat) -> UIColor {
-        let red = CGFloat.random(in: 0...1)
-        let green = CGFloat.random(in: 0...1)
-        let blue = CGFloat.random(in: 0...1)
+        let red = CGFloat.random(in: 0.5...1)
+        let green = CGFloat.random(in: 0.5...1)
+        let blue = CGFloat.random(in: 0.5...1)
         
         return UIColor(red: red, green: green, blue: blue, alpha: alpha)
     }
