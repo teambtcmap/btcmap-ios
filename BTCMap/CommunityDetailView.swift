0bc05bcecb2ae427ee516aab92255d8131df7186
Berserk-21 <cryptogekkofr@protonmail.com>
2023-06-11 22:02:07 +0200

    Change the black default background to clear for "No locations" text

--- BTCMap/CommunityDetailView.swift
+++ BTCMap/CommunityDetailView.swift
@@ -89,6 +89,7 @@ struct CommunityDetailView: View {
                     .listRowSeparator(.hidden)
                     .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
                     .padding([.top, .bottom], 6)
+                    .listRowBackground(Color.clear)
             }
             
             ForEach(filteredElements) { item in
